25b21c2f26a243398fc4dcff0c17984743ff7107
zhaxq <zhaxq000@gmail.com>
2025-02-02 14:24:27 +0000

    config change token

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-TOKEN = "MTMzMzQ3Mjk2MjM4ODY4ODk2Ng.G165jU.vg-P2H_Hh0q25bzIA0lm1ND1IQZyUsgKLkMeFg"
+import os
+
+TOKEN = os.getenv("TOKEN")  # Obtiene el token desde las variables de entorno
 GENERAL_CHANNEL_ID = 1333473708538331169  # Canal donde se enviará el mensaje
 BOTCOMMANDS_ROLE_ID = 1333479147082612938  # ID del rol de admin
 REPOST_CHANNEL_ID = 1333478891267817586  # ID del canal donde se publicará el embed
